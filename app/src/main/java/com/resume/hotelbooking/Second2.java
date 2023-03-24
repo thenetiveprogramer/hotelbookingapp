9db1d86c33eb1ba0f060fabc29d9a78d0c2bbd98
thenativeprogramer <swati14598@gmail.com>
2023-03-24 09:37:17 +0530

    Initial commit

---
+++ app/src/main/java/com/resume/hotelbooking/Second2.java
@@ -0,0 +1,14 @@
+package com.resume.hotelbooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Second2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second2);
+    }
+}
